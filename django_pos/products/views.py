27d169512995047cc8360f1d8bb7676466b720b8
JoseAlejandro <1412163@senati.pe>
2023-12-29 09:57:21 -0500

    Fixing the update product view

--- django_pos/products/views.py
+++ django_pos/products/views.py
@@ -200,66 +200,58 @@ def ProductsAddView(request):
     return render(request, "products/products_add.html", context=context)
 
 
-
 @login_required(login_url="/accounts/login/")
 def ProductsUpdateView(request, product_id):
-    """
-    Args:
-        product_id : The product's ID that will be updated
-    """
-
-    # Get the product
     try:
         # Get the product to update
         product = Product.objects.get(id=product_id)
+
+        if request.method == 'POST':
+            # Delete existing ProductoCategoria instances for this product
+            ProductoCategoria.objects.filter(id_producto=product).delete()
+
+            # Add new ProductoCategoria instances for selected categories
+            selected_categories = request.POST.getlist('category')
+            for category_id in selected_categories:
+                category = Category.objects.get(pk=category_id)
+                ProductoCategoria.objects.create(id_producto=product, id_categoria=category)
+
+            # Update product attributes
+            data = request.POST
+            product.name = data['name']
+            product.status = data['state']
+            product.description = data['description']
+            product.price = data['price']
+            product.save()
+
+            messages.success(request, f'Product: {product.name} updated successfully!', extra_tags="success")
+            return redirect('products:products_list')
+
+    except Product.DoesNotExist as e:
+        messages.error(request, 'Product not found!', extra_tags="danger")
+        print(e)
+        return redirect('products:products_list')
     except Exception as e:
-        messages.success(
-            request, 'There was an error trying to get the product!', extra_tags="danger")
+        messages.error(request, f'Error updating product: {e}', extra_tags="danger")
         print(e)
         return redirect('products:products_list')
 
+    categories = Category.objects.all()
+    product_categories = product.productocategoria_set.values_list('id_categoria', flat=True)
+
     context = {
         "active_icon": "products",
-        "product_status": Product.status.field.choices,
+        "product_status": Product.STATUS_CHOICES,  # Use STATUS_CHOICES from the model
         "product": product,
-        "categories": Category.objects.all()
+        "categories": categories,
+        "product_categories": product_categories,
     }
 
-    if request.method == 'POST':
-        try:
-            # Save the POST arguements
-            data = request.POST
-
-            attributes = {
-                "name": data['name'],
-                "status": data['state'],
-                "description": data['description'],
-                "category": Category.objects.get(id=data['category']),
-                "price": data['price']
-            }
-
-            # Check if a product with the same attributes exists
-            if product.objects.filter(**attributes).exists():
-                messages.error(request, 'Product already exists!',
-                               extra_tags="warning")
-                return redirect('products:products_add')
-
-            # Get the product to update
-            product = Product.objects.filter(
-                id=product_id).update(**attributes)
-
-            product = Product.objects.get(id=product_id)
-
-            messages.success(request, '¡Product: ' + product.name +
-                             ' updated successfully!', extra_tags="success")
-            return redirect('products:products_list')
-        except Exception as e:
-            messages.success(
-                request, 'There was an error during the update!', extra_tags="danger")
-            print(e)
-            return redirect('products:products_list')
-
     return render(request, "products/products_update.html", context=context)
+
+
+
+
 
 
 @login_required(login_url="/accounts/login/")
